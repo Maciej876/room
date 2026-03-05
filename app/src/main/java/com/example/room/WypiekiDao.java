a0e0dec3b64ad5b857a176d7521631b075301a8b
Maciej876 <maciek.falek4@gmail.com>
2026-03-06 00:20:45 +0100

    Update WypiekiDao.java

--- app/src/main/java/com/example/room/WypiekiDao.java
+++ app/src/main/java/com/example/room/WypiekiDao.java
@@ -9,20 +9,17 @@ import androidx.room.Update;
 import java.util.List;
 
 @Dao
-public interface WypiekiDao {
-
+public interface WypiekiDAO {
     @Insert
-    public void wstawWypiekDoBazy(Wypieki wypiek);
-
+    public void wstawWypiekDoBazy(Wypiek wypiek);
+    @Insert
+    public void wstawKilkaWybiekow(Wypiek ...wypieks);
     @Delete
-    public void usunZBazy(Wypieki wypiek);
-
+    public void usunWypiekZBazy(Wypiek wypiek);
     @Update
-    public void zaktualizuj(Wypieki wypiek);
-
-    @Query("Select * from wypieki_tabela")
-    List<Wypieki> zwrocWszystkieWypiekiZBazy();
-
-    @Query("Select nazwa from wypieki_tabela where czas_pieczenia< :czas")
+    public void zaktualizujWypiek(Wypiek wypiek);
+    @Query ("Select * from wypieki_tabela")
+    List<Wypiek> zwrocWszystkieWypiekiZBazy();
+    @Query("Select nazwaWypieku from wypieki_tabela Where `czas pieczenia`< :czas")
     List<String> zwrocNazwyWypiekow(int czas);
 }
