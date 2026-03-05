d94fd01d30cce3e627e5d6cca6360a548033dffa
Maciej876 <maciek.falek4@gmail.com>
2026-03-06 00:18:52 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/room/MainActivity.java
+++ app/src/main/java/com/example/room/MainActivity.java
@@ -14,55 +14,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.lang.reflect.Array;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    PrzepisyDataBase przepisyDataBase;
+    /* https://developer.android.com/jetpack/androidx/releases/room?hl=pl#groovy */
+    PrzepisyDatabase przepisyDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        przepisyDataBase = PrzepisyDataBase.zwrocInstancjeBazyDanych(MainActivity.this);
-        przepisyDataBase.zwrocWypiekiDao().wstawWypiekDoBazy(new Wypieki("sernik", "ser, ziemniaki, cukier, jajka", 200, 170));
-        przepisyDataBase.zwrocWypiekiDao().wstawWypiekDoBazy(new Wypieki("drożdżówki", "ser, ziemniaki, cukier, jajka", 200, 170));
-        przepisyDataBase.zwrocWypiekiDao().wstawWypiekDoBazy(new Wypieki("szarotka", "ser, ziemniaki, cukier, jajka", 200, 170));
-        ListView listView = findViewById(R.id.listView);
-        Button button = findViewById(R.id.button);
-        EditText editText = findViewById(R.id.editTextText);
-        EditText editText1 = findViewById(R.id.editTextText2);
-        EditText editText2 = findViewById(R.id.editTextText3);
-        EditText editText3 = findViewById(R.id.editTextText4);
-        List<Wypieki> wszystkieWypiekiList = przepisyDataBase
-                .zwrocWypiekiDao()
-                .zwrocWszystkieWypiekiZBazy();
-        ArrayAdapter<Wypieki> arrayAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                wszystkieWypiekiList
-        );
+        przepisyDatabase = PrzepisyDatabase.zwrocInstancjeBazyDanych(MainActivity.this);
+        /*przepisyDatabase.zwrocWypiekiDao().wstawWypiekDoBazy(new Wypiek(170, 120, "Sernik", "ser, ziemniaki, cukier, jajka"));
+        przepisyDatabase.zwrocWypiekiDao().wstawWypiekDoBazy(new Wypiek(170, 15, "drożdzówki", "ser, drożdże, mąka, cukier, jajka"));
+        przepisyDatabase.zwrocWypiekiDao().wstawWypiekDoBazy(new Wypiek(170, 60, "Chleb", "mąka, drożdże"));
+        przepisyDatabase.zwrocWypiekiDao().zwrocWszystkieWypiekiZBazy();*/
+        ListView listView = findViewById(R.id.listview);
+        List<Wypiek> wszystkieWypiekiListy = przepisyDatabase.zwrocWypiekiDao().zwrocWszystkieWypiekiZBazy();
+        ArrayAdapter<Wypiek> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, wszystkieWypiekiListy);
         listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        przepisyDataBase.zwrocWypiekiDao().usunZBazy(wszystkieWypiekiList.get(i));
-                        wszystkieWypiekiList.remove(i);
-                        arrayAdapter.notifyDataSetChanged();
-                    }
-                }
-        );
-        button.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String nazwa = editText.getText().toString();
-                        String skladniki = editText1.getText().toString();
-                        String temperatura = editText2.getText().toString();
-                        String czas = editText3.getText().toString();
-
-                    }
-                }
-        );
-    };
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                przepisyDatabase.zwrocWypiekiDao().usunWypiekZBazy(wszystkieWypiekiListy.get(i));
+                wszystkieWypiekiListy.remove(i);
+                arrayAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+        EditText nazwaWypieku = findViewById(R.id.nazwa_wypieku);
+        EditText skladniki = findViewById(R.id.skladniki);
+        EditText czasWypieku = findViewById(R.id.czas_pieczenia);
+        EditText temperaturaPieczenia = findViewById(R.id.temperatura_pieczenia);
+        Button przyciskDodaj = findViewById(R.id.przycisk_dodaj);
+        Button przyciskEdytuj = findViewById(R.id.przycisk_edytuj);
+        przyciskDodaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                przepisyDatabase.zwrocWypiekiDao().wstawWypiekDoBazy(new Wypiek(Integer.parseInt(temperaturaPieczenia.getText().toString()), Integer.parseInt(czasWypieku.getText().toString()), nazwaWypieku.getText().toString(), skladniki.getText().toString()));
+                wszystkieWypiekiListy.add(new Wypiek(Integer.parseInt(temperaturaPieczenia.getText().toString()), Integer.parseInt(czasWypieku.getText().toString()), nazwaWypieku.getText().toString(), skladniki.getText().toString()));
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Wypiek wypiek = wszystkieWypiekiListy.get(i);
+                nazwaWypieku.setText(wypiek.getNazwaWypieku().toString());
+                skladniki.setText(wypiek.getSkladniki().toString());
+                temperaturaPieczenia.setText(Integer.toString(wypiek.getTemperaturaPieczenia()));
+                czasWypieku.setText(Integer.toString(wypiek.getCzasPieczenia()));
+            }
+        });
     }
+}
